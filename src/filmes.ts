70ff09d594a47ea112b36f7a946fdd49889fe345
THAINA OLIVEIRA ARAUJO <thaina.oaraujo1@senacsp.edu.br>
2022-10-05 09:17:47 -0300

    first commit

---
+++ src/filmes.ts
@@ -0,0 +1,3 @@
+let filmes = ['O iluminado','Dr.Sonho','The Black Phone','Men','Umma','Smile','The Humans','Fresh']
+
+export default filmes
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/* import Matematica from './matematica'
+
+let n1: number = 10
+let n2: number = 20
+
+console.log("SOMA: "+Matematica.somar(n1,n2))
+console.log("SUBTRAIR: "+Matematica.subtrair(n1,n2))
+console.log("multiplicar: "+Matematica.multiplicar(n1,n2)) */
+
+import Filmes from './filmes'
+
+console.log(Filmes)
